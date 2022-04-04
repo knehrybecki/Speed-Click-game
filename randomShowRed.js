39b1d8b7a4e6978f21c3238f7404ea6c776302a0
Kamil Nehrybecki <knehrybecki@gmail.com>
2022-04-05 00:39:32 +0200

    game

---
+++ randomShowRed.js
@@ -0,0 +1,20 @@
+import { boxCircle } from "./script.js"
+
+const getRandomNumberBetween = (min,max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+export const randomShowRed = () => {
+    setInterval(() => {
+        const randomShow = boxCircle[Math.floor(Math.random() * boxCircle.length)]
+
+        randomShow.classList.add('red')
+
+        setTimeout(() => {
+            randomShow.classList.remove('red')
+            randomShow.classList.remove('green')
+
+            randomShow.style.pointerEvents = null
+        }, 1000);
+    }, getRandomNumberBetween(1000, 5000))
+}
